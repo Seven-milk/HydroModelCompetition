78c64db897d7d3b3bf03f7a80948b6efdf307955
XudongZhengSteven <z786909151@163.com>
2021-05-14 18:59:03 +0800

    modify

--- runoff_predict.py
+++ runoff_predict.py
@@ -13,6 +13,10 @@ import matplotlib.pyplot as plt
 home = 'F:/文件/水科学数值模拟大赛/prelim/attribute_target'
 attribute_train = np.load(os.path.join(home, 'attribute_train.npy'))
 target_train = np.load(os.path.join(home, 'target_train.npy'))
+attribute_test = np.load(os.path.join(home, 'attribute_test.npy'))
+target_test = np.load(os.path.join(home, 'target_test.npy'))
+model_path = 'F:/文件/水科学数值模拟大赛/prelim/model1'
+checkpoint_save_path = os.path.join(model_path, 'checkpoint/mnist.ckpt')
 
 # shuffle train data
 np.random.seed(10)
@@ -38,12 +42,26 @@ class RunoffFCNN(Model):
         return y
 
 
+# model set
 runoffmodel = RunoffFCNN()
 runoffmodel.compile(optimizer='adam', loss="mse", metrics=['accuracy'])
-history = runoffmodel.fit(attribute_train, target_train, batch_size=32, epochs=1000, validation_split=0.2,
-                          validation_freq=1)
-runoffmodel.summary()
 
+
+# load model
+if os.path.exists(checkpoint_save_path + '.index'):
+    print('------------------- load model -------------------')
+    runoffmodel.load_weights(checkpoint_save_path)
+
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_save_path,
+                                                 save_weights_only=True,
+                                                 save_best_only=True)
+
+
+# model fit
+history = runoffmodel.fit(attribute_train, target_train, batch_size=32, epochs=5, validation_split=0.2,
+                      validation_freq=1, callbacks=[cp_callback])
+
+# plot
 acc = history.history["accuracy"]
 val_acc = history.history["val_accuracy"]
 loss = history.history["loss"]
@@ -61,3 +79,35 @@ plt.title("Training and Validation Loss")
 
 plt.legend()
 plt.show()
+
+runoffmodel.summary()
+print(runoffmodel.trainable_variables)
+with open(os.path.join(model_path, 'weights.txt'), 'w') as file:
+    for v in runoffmodel.trainable_variables:
+        file.write(str(v.name) + '\n')
+        file.write(str(v.shape) + '\n')
+        file.write(str(v.numpy()) + '\n')
+
+
+# predict
+predict_on = input('whether predict, False or True')
+target_predict = runoffmodel.predict(attribute_test)
+np.save("target_predict", target_predict)
+
+
+# predict plot
+def predict_plot():
+    predict_num = 16
+    fig, ax = plt.subplots(4, 4, sharex=True, sharey=False)
+    ax = ax.flatten()
+    for i in range(predict_num):
+        rand_ = np.random.randint(0, len(target_train))
+        predict_ = runoffmodel.predict(attribute_train[rand_, :].reshape(1, 620))
+        ax[i].plot(predict_.reshape(7, ), 'r', label="predict")
+        ax[i].plot(target_train[rand_, :], 'b', label="real data")
+        print("mse: ", sum((predict_ - target_train[rand_, :]).flatten()**2))
+        ax[i].legend(loc="upper right")
+        ax[i].set_xlabel("days")
+        ax[i].set_ylim(0, 1)
+
+predict_plot()
